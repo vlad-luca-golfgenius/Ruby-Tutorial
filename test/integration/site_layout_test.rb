3f79d783ee9727065ccd4c0c8e0871018e6a98b3
Vlad Luca <vladluca@Vlads-MacBook-Pro.local>
2022-06-30 17:34:55 +0300

    solved yarn and lock mismatch

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path, count: 2
     assert_select "a[href=?]", contact_path, count: 2
     assert_select "a[href=?]", signup_path, count: 1
+
+
   end
 end
