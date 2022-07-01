84acff9a2ecc23b4f564566d312d9f7efdbdbc6d
Vlad Luca <vladluca@Vlads-MacBook-Pro.local>
2022-07-01 10:38:05 +0300

    Add user model  and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_080927) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_07_01_072146) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
